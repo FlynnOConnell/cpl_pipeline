f187d6470796c121ff01648d22a4570f2246ec7f
Flynn OConnell <flynnoconnell@gmail.com>
2023-09-30 18:13:40 -0400

    more io, versioning

---
+++ spk2extract/io/__init__.py
@@ -0,0 +1,11 @@
+from .server import ssh_connect
+from .spk_io import read_h5, write_h5
+from .utils import (
+    get_spk2extract_path,
+    search_for_ext,
+    get_h5_list,
+    get_sbx_list,
+    list_h5,
+    list_files,
+    init_params,
+)
---
+++ spk2extract/version.py
@@ -0,0 +1,3 @@
+from importlib_metadata import metadata as _metadata
+
+version = _metadata("spk2extract")["version"]
