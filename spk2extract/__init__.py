bf15eeafb28485e668fd17111571aea8ff00b712
Flynn OConnell <flynnoconnell@gmail.com>
2023-10-01 16:06:39 -0400

    better directory management

--- spk2extract/__init__.py
+++ spk2extract/__init__.py
@@ -13,14 +13,46 @@ I recommend exploring the docstrings using
 TAB-completion and introspection capabilities.
 
 """
-from .extraction import SpikeData, UnitData  # noqa (API import)
-from .gui import *  # noqa (API import)
-from .util import *  # noqa (API import)
-from .version import version as __version__  # noqa (API import)
-from .defaults import defaults  # noqa (API import)
+from platformdirs import *
+    # user_data_dir,
+    # user_cache_dir,
+    # user_config_dir,
+    # user_log_dir,
+  # noqa (API import)
+
+from spk2extract.defaults import defaults  # noqa (API import)
+from spk2extract.extraction import SpikeData, UnitData  # noqa (API import)
+from spk2extract.gui import *  # noqa (API import)
+from spk2extract.spk_io import * # noqa (API import)
+from spk2extract.util import *  # noqa (API import)
+from spk2extract.version import version as __version__  # noqa (API import)
 
 version = __version__
-# Documentation inclusions
-__all__ = ["SpikeData", "UnitData", "extract_waveforms", "dejitter", "filter_signal", "defaults", "version", "gui"]
 __name__ = "spk2extract"
 __author__ = "Flynn OConnell"
+
+# Platform-dependent directories
+spk2dir = Path().home() / "spk2extract"
+if not spk2dir.exists():
+    spk2dir.mkdir(exist_ok=True)
+cache_dir = user_cache_dir(__name__, __author__)  # Cache, temp files
+config_dir = user_config_dir(__name__, __author__)  # Config, parameters and options
+log_dir = user_log_dir(__name__, __author__)  # Logs, .log files primarily
+
+# Documentation inclusions
+__all__ = [
+    "SpikeData",
+    "UnitData",
+    "spk_io",
+    "extract_waveforms",
+    "dejitter",
+    "filter_signal",
+    "defaults",
+    "version",
+    "gui",
+    "cache_dir",
+    "config_dir",
+    "log_dir",
+    "spk2dir",
+]
+x = 5
