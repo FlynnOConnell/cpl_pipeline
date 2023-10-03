79535f37d26d24fdee0e6a386c5c9d6c08dbf887
Flynn OConnell <FlynnOConnell@gmail.com>
2023-10-03 01:31:35 -0400

    remove unused funcs

--- spk2extract/spk_io/__init__.py
+++ spk2extract/spk_io/__init__.py
@@ -1,17 +1,6 @@
 from .server import ssh_connect
-from .utils import (
-    get_spk2extract_path,
-    search_for_ext,
-    get_sbx_list,
-    list_h5,
-    list_files,
-)
+
 
 __all__ = [
     "ssh_connect",
-    "get_spk2extract_path",
-    "search_for_ext",
-    "get_sbx_list",
-    "list_h5",
-    "list_files",
 ]
