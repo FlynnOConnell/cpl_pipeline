b87f1fc446ef474242bfe93cd3b02bd162daef20
Flynn OConnell <flynnoconnell@gmail.com>
2023-10-19 12:20:11 -0400

    coherence mortet

--- spk2extract/extraction/__init__.py
+++ spk2extract/extraction/__init__.py
@@ -1,3 +1,155 @@
 from .spike2 import Spike2Data, WaveData
 
 __all__ = ["Spike2Data", "WaveData"]
+#       fig, ax = plt.subplots(4, 1, figsize=(10, 20), sharex=True)
+#         fig.suptitle(
+#             f"Beta Filtered LFP: {letter_map.get(letter_str)}, {digit_map.get(digit_str)}",
+#             fontsize=20,
+#             fontweight="bold",
+#             y=0.95,
+#             x=0.5,
+#             ha="center",
+#             va="top",
+#         )
+# lfp = LfpSignal(
+#     resampled_lfp_df,
+#     1000,
+#     event_arr=ev,
+#     ev_times_arr=event_times,
+#     filename=file,
+#     exclude=["LFP1_AON", "LFP2_AON"],
+# )
+# lfp.filter(15, 100)
+# lfp.notch()
+# windowed_data = {}
+#
+# letter_map = {"b": "Black", "w": "White"}
+# digit_map = {"0": "Incorrect dig", "1": "Correct dig"}
+#
+# x = 0
+# for letter_str, digit_str, spikes_window in lfp.get_windows(0.5, 0.5, "all"):
+#     x += 1
+#     this_window = {}
+#
+#     fig, ax = plt.subplots(4, 1, figsize=(10, 20), sharex=True)
+#     fig.suptitle(
+#         f"Beta Filtered LFP: {letter_map.get(letter_str)}, {digit_map.get(digit_str)}",
+#         fontsize=20,
+#         fontweight="bold",
+#         y=0.95,
+#         x=0.5,
+#         ha="center",
+#         va="top",
+#     )
+#
+#     channel_pairs = [("LFP1_vHp", "LFP3_AON"), ("LFP2_vHp", "LFP4_AON")]
+#
+#     ax_idx = 0
+#     for ch1, ch2 in channel_pairs:
+#         beta_filtered_list = []
+#         for channel in [ch1, ch2]:
+#             channel_data = {"raw": spikes_window[channel]}
+#
+#             idx_min, idx_max = spikes_window.index.min(), spikes_window.index.max()
+#             xticks = np.linspace(idx_min, idx_max, 11)
+#             xticklabels = np.linspace(0, (idx_max - idx_min) / 2000, 11).round(2)
+#
+#             ymin, ymax = spikes_window[channel].min(), spikes_window[channel].max()
+#
+#             beta_filtered = butter_bandpass_filter(
+#                 spikes_window[channel], 12, 30, fs=1000
+#             )
+#             gamma_filtered = butter_bandpass_filter(
+#                 spikes_window[channel], 30, 80, fs=1000
+#             )
+#             theta_filtered = butter_bandpass_filter(
+#                 spikes_window[channel], 4, 12, fs=1000
+#             )
+#             channel_data["beta"] = beta_filtered
+#             channel_data["gamma"] = gamma_filtered
+#             channel_data["theta"] = theta_filtered
+
+# ax[ax_idx].plot(
+#     spikes_window.index,
+#     spikes_window[channel],
+#     label="Raw",
+#     color="gray",
+#     linewidth=1,
+#     alpha=0.5,
+#     zorder=1,
+# )
+# ax[ax_idx].plot(
+#     spikes_window.index,
+#     beta_filtered,
+#     label="Beta",
+#     color="black",
+#     linewidth=2,
+#     zorder=2,
+# )
+#
+# ax[ax_idx].set_title(f"{channel}")
+# ax[ax_idx].set_xticks(xticks)
+# ax[ax_idx].set_xticklabels(xticklabels)
+# ax[ax_idx].set_ylabel("Amplitude (V)")
+# ax[ax_idx].set_xlim(idx_min, idx_max)
+# ax[ax_idx].set_ylim(ymin, ymax)
+# ax[ax_idx].legend()
+# ax_idx += 1
+#
+# lfp = LfpSignal(
+#     resampled_lfp_df,
+#     1000,
+#     event_arr=ev,
+#     ev_times_arr=event_times,
+#     filename=file,
+#     exclude=["LFP1_AON", "LFP2_AON"],
+# )
+# lfp.filter(15, 100)
+# lfp.notch()
+# windowed_data = {}
+#
+# letter_map = {"b": "Black", "w": "White"}
+# digit_map = {"0": "Incorrect dig", "1": "Correct dig"}
+#
+# x = 0
+# for letter_str, digit_str, spikes_window in lfp.get_windows(0.5, 0.5, "all"):
+#     x += 1
+#     this_window = {}
+#
+#     fig, ax = plt.subplots(4, 1, figsize=(10, 20), sharex=True)
+#     fig.suptitle(
+#         f"Beta Filtered LFP: {letter_map.get(letter_str)}, {digit_map.get(digit_str)}",
+#         fontsize=20,
+#         fontweight="bold",
+#         y=0.95,
+#         x=0.5,
+#         ha="center",
+#         va="top",
+#     )
+#
+#     channel_pairs = [("LFP1_vHp", "LFP3_AON"), ("LFP2_vHp", "LFP4_AON")]
+#
+#     ax_idx = 0
+#     for ch1, ch2 in channel_pairs:
+#         beta_filtered_list = []
+#         for channel in [ch1, ch2]:
+#             channel_data = {"raw": spikes_window[channel]}
+#
+#             idx_min, idx_max = spikes_window.index.min(), spikes_window.index.max()
+#             xticks = np.linspace(idx_min, idx_max, 11)
+#             xticklabels = np.linspace(0, (idx_max - idx_min) / 2000, 11).round(2)
+#
+#             ymin, ymax = spikes_window[channel].min(), spikes_window[channel].max()
+#
+#             beta_filtered = butter_bandpass_filter(
+#                 spikes_window[channel], 12, 30, fs=1000
+#             )
+#             gamma_filtered = butter_bandpass_filter(
+#                 spikes_window[channel], 30, 80, fs=1000
+#             )
+#             theta_filtered = butter_bandpass_filter(
+#                 spikes_window[channel], 4, 12, fs=1000
+#             )
+#             channel_data["beta"] = beta_filtered
+#             channel_data["gamma"] = gamma_filtered
+#             channel_data["theta"] = theta_filtered
--- spk2extract/extraction/lfp.py
+++ spk2extract/extraction/lfp.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
-from collections import defaultdict
 from pathlib import Path
 from typing import Generator
 
 import mne
+import mne_connectivity
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
-from scipy.fft import fft
 from scipy.signal import decimate, butter, filtfilt
 
 from spk2extract.logs import logger
@@ -194,15 +193,36 @@ class LfpSignal:
 
 
 if __name__ == "__main__":
+    # Set up 'ggplot' style
+    plt.style.use("ggplot")
+
+    # Configure parameters for publication-ready graphs
+    plt.rcParams["axes.labelsize"] = 20  # Label size
+    plt.rcParams["axes.titlesize"] = 22  # Title size
+    plt.rcParams["xtick.labelsize"] = 14  # x-axis tick label size
+    plt.rcParams["ytick.labelsize"] = 14  # y-axis tick label size
+    plt.rcParams["legend.fontsize"] = 16  # Legend font size
+    plt.rcParams["lines.linewidth"] = 2  # Line width
+    plt.rcParams["axes.titleweight"] = "bold"  # Title weight
+    plt.rcParams["axes.labelweight"] = "bold"  # Label weight
+    plt.rcParams["axes.spines.top"] = False  # Remove top border
+    plt.rcParams["axes.spines.right"] = False  # Remove right border
+    plt.rcParams["axes.spines.left"] = True
+    plt.rcParams["axes.spines.bottom"] = True
+    plt.rcParams["font.weight"] = "bold"
+    plt.rcParams["font.family"] = "sans-serif"
+    plt.rcParams["text.color"] = "black"
+
     data_path = Path().home() / "data" / "extracted"
+    save_path = Path().home() / "data" / "figures"
     file = list(data_path.glob("*0609*.h5"))[0]
     df_s, df_t, ev, event_times = get_data(file)
-    q = 2  # Decimation factor; 2000 Hz / 2 = 1000 Hz
-    resampled_lfp_df = df_s.apply(lambda col: decimate(col, q), axis=0)
+    # resampled to 1000Hz
+    resampled_lfp_df = df_s.apply(lambda col: decimate(col, 2), axis=0)
 
     lfp = LfpSignal(
         resampled_lfp_df,
-        2000,
+        1000,
         event_arr=ev,
         ev_times_arr=event_times,
         filename=file,
@@ -210,64 +230,95 @@ if __name__ == "__main__":
     )
     lfp.filter(15, 100)
     lfp.notch()
+    letter_map = {"b": "Black", "w": "White"}
+    digit_map = {"0": "Incorrect dig", "1": "Correct dig"}
 
-    x = 0
-    channel_data = defaultdict(list)
+    data_by_trial_type = {}
+    info = mne.create_info(
+        ch_names=list(lfp.spikes.columns), sfreq=lfp.fs, ch_types="eeg"
+    )
+    data = []
+    metadata_list = []
 
     for letter_str, digit_str, spikes_window in lfp.get_windows(0.5, 0.5, "all"):
-        x += 1
-        if x > 10:
-            break
+        trial_key = f"{letter_str}_{digit_str}"
+        if trial_key not in data_by_trial_type:
+            data_by_trial_type[trial_key] = {}
+        this_window = {}
+        channel_pairs = [("LFP1_vHp", "LFP3_AON"), ("LFP2_vHp", "LFP4_AON")]
+        for ch1, ch2 in channel_pairs:
+            for channel in [ch1, ch2]:
+                channel_data = {
+                    "raw": spikes_window[channel].to_numpy(),
+                    "beta": butter_bandpass_filter(
+                        spikes_window[channel], 12, 30, fs=1000
+                    ),
+                    "gamma": butter_bandpass_filter(
+                        spikes_window[channel], 30, 80, fs=1000
+                    ),
+                    "theta": butter_bandpass_filter(
+                        spikes_window[channel], 4, 12, fs=1000
+                    ),
+                }
+                this_window[channel] = channel_data
 
-        for channel in spikes_window.columns:
-            channel_data[channel].append((letter_str, digit_str, spikes_window))
+        epoch_data = np.array(
+            [this_window[channel]["raw"] for channel in list(lfp.spikes.columns)]
+        )
+        data.append(epoch_data)
+        metadata_list.append({"letter": letter_str, "digit": digit_str})
 
-    for channels in [("LFP1_vHp", "LFP3_AON"), ("LFP2_vHp", "LFP4_AON")]:
-        fig, ax = plt.subplots(2, 1, figsize=(10, 15))
+        data_by_trial_type[trial_key][len(data_by_trial_type[trial_key])] = this_window
 
-        for i, channel in enumerate(channels):
-            for letter_str, digit_str, spikes_window in channel_data[channel]:
-                # Similar processing as your code
-                idx_min, idx_max = spikes_window.index.min(), spikes_window.index.max()
-                xticks = np.linspace(idx_min, idx_max, 11)
-                xticklabels = np.linspace(0, (idx_max - idx_min) / 2000, 11).round(2)
+    # Make mne epochs for each window based on the shortest window
+    min_len = min([d.shape[1] for d in data])
+    shortened = []
+    for d in data:
+        size = d.shape[1]
+        d = d[:, size - min_len :]
+        shortened.append(d)
 
-                beta_filtered = butter_bandpass_filter(
-                    spikes_window[channel], 12, 30, fs=2000
-                )
-                gamma_filtered = butter_bandpass_filter(
-                    spikes_window[channel], 30, 100, fs=2000
-                )
+    data_array = np.stack(shortened, axis=0)
+    epochs = mne.EpochsArray(data_array, info)
+    epochs.metadata = pd.DataFrame(metadata_list)
 
-                ax[i].plot(
-                    spikes_window.index,
-                    spikes_window[channel],
-                    label="Raw",
-                    color="gray",
-                    alpha=0.5,
-                    zorder=1,
-                )
-                ax[i].plot(
-                    spikes_window.index,
-                    beta_filtered,
-                    label="Beta",
-                    color="r",
-                    zorder=2,
-                )
-                ax[i].plot(
-                    spikes_window.index,
-                    gamma_filtered,
-                    label="Gamma",
-                    color="g",
-                    zorder=3,
-                )
-                ax[i].set_title(f"{letter_str}_{digit_str} Channel: {channel}")
-                ax[i].set_xticks(xticks)
-                ax[i].set_xticklabels(xticklabels)
-                ax[i].set_xlabel("Time (s)")
-                ax[i].legend()
+    # Now, you can easily filter epochs based on metadata
+    ch1 = "LFP2_vHp"
+    ch2 = "LFP4_AON"
+    epochs_b1 = epochs['letter == "b" and digit == "1"']
+    index_lfp1 = epochs_b1.ch_names.index(ch1)
+    index_lfp3 = epochs_b1.ch_names.index(ch2)
+    data_b1 = epochs_b1.get_data()
+    two_channel_data = data_b1[:, [index_lfp1, index_lfp3], :]
+    freqs = np.arange(1, 100, 1)
+    n_cycles = freqs / 2.0
 
-        plt.subplots_adjust(hspace=0.5)
-        plt.show()
+    con = mne_connectivity.spectral_connectivity_epochs(
+        two_channel_data,
+        method="coh",
+        sfreq=int(lfp.fs),
+        mode="cwt_morlet",
+        cwt_freqs=freqs,
+        cwt_n_cycles=n_cycles,
+        verbose=True,
+        indices=(np.array([0]), np.array([1])),
+    )
 
-    x = 4
+    coh = con.get_data() # The coherence matrix, shape will be (n_freqs, n_times)
+    times = epochs_b1.times
+    plt.imshow(
+        np.squeeze(coh),
+        extent=(times[0], times[-1], freqs[0], freqs[-1]),
+        aspect="auto",
+        origin="lower",
+        cmap="jet",
+    )
+    plt.title(f"{ch1} vs {ch2} \n"
+              f"Black, Correct", fontsize=14)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Frequency (Hz)")
+    # colorbar units are "coherence"
+    plt.colorbar(label="Coherence")
+    plt.savefig(save_path / "coh.png", dpi=300, bbox_inches="tight", pad_inches=0.1, transparent=True)
+
+    x = 2
