73f91a56d06e21b570eb34b11eec9e69ed0ac1b1
Flynn OConnell <flynnoconnell@gmail.com>
2023-12-23 23:11:32 -0500

    add args for new/load

--- cpl_extract/__main__.py
+++ cpl_extract/__main__.py
@@ -19,9 +19,6 @@ def add_args(parser: argparse.ArgumentParser):
 
     parser.add_argument("-y", "--yes", action="store_true", help="Auto-accept all arguments.")
     parser.add_argument("-P", "--parallel", action="store_true", help="Run in parallel.")
-    parser.add_argument("-d", "--detect-spikes", action="store_true", help="Detect spikes.")
-    parser.add_argument("-c", "--cluster-spikes", action="store_true", help="Cluster spikes.")
-    parser.add_argument("-s", "--sort-spikes", action="store_true", help="Sort spikes.")
     parser.add_argument("-a", "--all", action="store_true", help="Run all steps.")
 
     for step in Dataset.PROCESSING_STEPS:
@@ -33,82 +30,49 @@ def parse_args(parser: argparse.ArgumentParser):
     """ Parses arguments from parser. """
 
     args = parser.parse_args()
-    dargs = vars(args)
 
-    if args.new is not True:
+    if args.new:
+        ic(f"Creating new dataset at {args.new}")
         datapath = Path(args.new).expanduser().resolve()
-    elif args.file is not True:
+        data = Dataset(datapath)
+        if args.yes:
+            data.initialize_parameters(accept_params=True)
+        else:
+            data.initialize_parameters()
+    elif args.file:
+        ic(f"Loading dataset from {args.file}")
         datapath = Path(args.file).expanduser().resolve()
+        data = load_dataset(datapath)
     else:
         datapath = Path(defaults()["datapath"]).expanduser().resolve()
-
-    ic(f"Setting datapath to {datapath}")
-    dargs["path"] = datapath
-    if args.new:
-        data = Dataset(root_dir=datapath, shell=True)
-        dargs['data'] = data
-    elif args.file:
-        data = load_dataset(datapath, shell=True)
-        dargs['data'] = data
-    else:
-        ic(f"Searching for .p file in {args.path}")
-        if list(Path(args.path).glob("*.p")):  # if it's a non-empty list
-            data = load_dataset(args.path, )
+        if datapath.is_dir():
+            data = Dataset(datapath)
         else:
-            raise FileNotFoundError(
-                f"No .p file found in {args.path}, please create a new dataset or load an existing one.")
+            data = load_dataset(datapath)
 
-    # Iterate over each processing step and execute if the corresponding flag is set
-    for step in Dataset.PROCESSING_STEPS:
-        arg = getattr(args, step.replace(' ', '_'))
-        if arg:
-            method_name = Dataset.PROCESSING_STEPS[step]
-            if method_name:
-                ic(f"Calling {method_name} from CLI args.")
-                method = getattr(data, method_name)
-                method()
-            else:
-                ic(f"No method found for argument provided: {arg}"
-                   f"Valid arguments: {Dataset.PROCESSING_STEPS.keys()}")
-
-    return args, dargs
+    # iterate over each processing step and execute if the corresponding flag is set
+    if args.all:
+        data.pre_process_for_clustering("initialize_parameters", accept_params=args.yes,)
+    else:
+        for i, step in enumerate(Dataset.PROCESSING_STEPS):
+            arg = getattr(args, step.replace(' ', '_'))
+            if arg:
+                method_name = Dataset.PROCESSING_STEPS[i]
+                if method_name:
+                    ic(f"Calling {method_name} from CLI args.")
+                    method = getattr(data, method_name)
+                    method()
+                else:
+                    ic(f"No method found for argument provided: {arg}"
+                           f"Valid arguments: {Dataset.PROCESSING_STEPS.keys()}")
+    return args, data
 
 
 def main(shell=False):
     if shell:
         parser = argparse.ArgumentParser(description="Sorting pipeline parameters.")
-        parser = add_args(parser)
-        args, dargs = parse_args(parser)
+        args, data = parse_args(add_args(parser))
 
-        data = dargs['data']
-        if not data.process_status['initialize_parameters']:
-            data.initialize_parameters()
-
-        if not data.process_status['extract_data']:
-            data.extract_data()
-
-        if not data.process_status['mark_dead_channels']:
-            data.mark_dead_channels()
-
-        if not data.process_status['spike_detection']:
-            data.detect_spikes()
-
-        if not data.process_status['spike_clustering']:
-            data.cluster_spikes()
-
-        if not data.process_status['cleanup_clustering']:
-            data.cleanup_clustering()
-
-        electrodes = data.electrode_mapping.loc[:, "electrode"].to_list()
-        for electrode in electrodes:
-            root, spikesort = data.sort_spikes(all_electrodes=electrodes)
-            x = 5
-        x = 5
-    # else:
-    #     filepath = Path().home() / "data" / 'serotonin' / '1'
-    #     things = [x for x in filepath.glob('*')]
-    #     file = [f for f in filepath.iterdir() if f.suffix == '.smr'][0]
-    #     data = load_dataset(args.path, shell=True, )
 
 
 if __name__ == "__main__":
